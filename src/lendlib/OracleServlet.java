74b8ffc873daca613d3a526c278992a6364286a2
kugurst <ma2799@gmail.com>
2013-04-11 15:29:36 -0400

    Trying to get web.xml to recognize OracleServlet. Efforst have been proving less than fruitful.

---
+++ src/lendlib/OracleServlet.java
@@ -0,0 +1,67 @@
+package lendlib;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import oracle.jdbc.pool.OracleDataSource;
+
+/**
+ * Servlet implementation class OracleServlet
+ */
+public class OracleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String connect_string = "jdbc:oracle:thin:ma2799/EiVQBUGs@//w4111c.cs.columbia.edu:1521/ADB";
+	private Connection conn;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public OracleServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = new PrintWriter(response.getOutputStream());
+		try {
+			if (conn == null) {
+				// Create a OracleDataSource instance and set URL
+				OracleDataSource ods = new OracleDataSource();
+				ods.setURL(connect_string);
+				conn = ods.getConnection();
+			}
+			Statement stmt = conn.createStatement();
+			ResultSet rset = stmt.executeQuery("select UNAME from PERSON");
+			response.setContentType("text/html");
+			pw.println("<html>");
+			pw.println("<head><title>Person Table Servlet Sample</title></head>");
+			pw.println("<H1>Show Person Table Data <BR></H1>");
+			pw.println("<body><BR>");
+			while (rset.next()) {
+				pw.println(rset.getString("uname") + "<BR>");
+			}
+			pw.println("</body></html>");
+		} catch (SQLException e) {
+			pw.println(e.getMessage());
+		}
+		pw.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+}
