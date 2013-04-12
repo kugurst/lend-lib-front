0798d922e8f7b608a61adafac07a39aadfdd72bc
kugurst <ma2799@gmail.com>
2013-04-11 22:45:37 -0400

    Validated that the form data that MakeUser.java receives is correct. Now adding logic.

--- src/lendlib/MakeUser.java
+++ src/lendlib/MakeUser.java
@@ -2,6 +2,9 @@ package lendlib;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.util.Arrays;
+import java.util.Map.Entry;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,9 +15,11 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MakeUser
  */
-@WebServlet(description = "Makes a user using the form action from login.jsp", urlPatterns = { "/makeuser" })
+@WebServlet("/makeuser")
 public class MakeUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String connect_string = "jdbc:oracle:thin:ma2799/EiVQBUGs@//w4111c.cs.columbia.edu:1521/ADB";
+	private Connection conn;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -31,8 +36,19 @@ public class MakeUser extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = new PrintWriter(response.getOutputStream());
+		// Check to see if the user already exists
+		String name = request.getParameter("username");
 		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>\n" + "<html>\n" + "<head>\n"
+				+ "<meta charset=\"ISO-8859-1\">\n"
+				+ "<title>Lib: Processing...</title>\n" + "</head>\n"
+				+ "<body>\n" + "\t<table>");
+		for (Entry<String, String[]> m : request.getParameterMap().entrySet())
+			out.println("\t\t<tr><td>" + m.getKey() + ": "
+					+ Arrays.toString(m.getValue()) + "</td></tr>");
+		out.println("\t</table>\n" + "</body>\n" + "</html>");
+		out.close();
 	}
 
 	/**
@@ -42,7 +58,7 @@ public class MakeUser extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
